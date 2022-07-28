28f19497ed3177bab3fa136692a29c1efa2f566f
anzhy.diz <anzhy.nesty@gmail.com>
2022-07-28 08:46:24 -0400

    first init

---
+++ app/src/main/java/anzhy/dizi/todojetpackcompose/MainActivity.kt
@@ -0,0 +1,24 @@
+package anzhy.dizi.todojetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import anzhy.dizi.todojetpackcompose.ui.theme.ToDoJetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ToDoJetpackComposeTheme {
+
+            }
+        }
+    }
+}
