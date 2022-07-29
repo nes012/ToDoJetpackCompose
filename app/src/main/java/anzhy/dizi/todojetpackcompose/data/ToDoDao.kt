895bc96eb70411b72dd703dbacea17c2af64288f
anzhy.diz <anzhy.nesty@gmail.com>
2022-07-29 16:19:43 -0400

    create repository

--- app/src/main/java/anzhy/dizi/todojetpackcompose/data/ToDoDao.kt
+++ app/src/main/java/anzhy/dizi/todojetpackcompose/data/ToDoDao.kt
@@ -11,7 +11,7 @@ interface ToDoDao {
     // runs asynchronously by default
 
     @Query("SELECT*FROM todo_table ORDER BY id ASC")
-    fun getAllTasks(): Flow<ToDoTask>
+    fun getAllTasks(): Flow<List<ToDoTask>>
 
     @Query("SELECT* FROM todo_table WHERE id=:taskId")
     fun getSelectedTask(taskId: Int): Flow<ToDoTask>
@@ -35,4 +35,17 @@ interface ToDoDao {
     //
     @Query("SELECT * FROM todo_table ORDER BY CASE WHEN priority LIKE 'L%' THEN 1 WHEN priority LIKE 'M%' THEN 2 WHEN priority LIKE 'H%' THEN 3 END")
     fun sortByLowPriority(): Flow<List<ToDoTask>>
+
+
+    @Query(
+        """
+        SELECT * FROM todo_table ORDER BY
+    CASE
+        WHEN priority LIKE 'H%' THEN 1
+        WHEN priority LIKE 'M%' THEN 2
+        WHEN priority LIKE 'L%' THEN 3
+    END
+    """
+    )
+    fun sortByHighPriority(): Flow<List<ToDoTask>>
 }
