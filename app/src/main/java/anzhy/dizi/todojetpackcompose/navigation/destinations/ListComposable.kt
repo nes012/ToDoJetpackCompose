97af67b1cb286a9dbd75d02efda3e5d34357079c
anzhy.diz <anzhy.nesty@gmail.com>
2022-08-29 16:08:58 -0400

    fix bug with config change

--- app/src/main/java/anzhy/dizi/todojetpackcompose/navigation/destinations/ListComposable.kt
+++ app/src/main/java/anzhy/dizi/todojetpackcompose/navigation/destinations/ListComposable.kt
@@ -4,15 +4,19 @@ import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
-import com.google.accompanist.navigation.animation.composable
 import androidx.navigation.navArgument
 import anzhy.dizi.todojetpackcompose.ui.screens.list.ListScreen
 import anzhy.dizi.todojetpackcompose.ui.viewmodels.SharedViewModel
+import anzhy.dizi.todojetpackcompose.utils.Action
 import anzhy.dizi.todojetpackcompose.utils.Constants.LIST_ARGUMENT_KEY
 import anzhy.dizi.todojetpackcompose.utils.Constants.LIST_SCREEN
 import anzhy.dizi.todojetpackcompose.utils.toAction
+import com.google.accompanist.navigation.animation.composable
 
 @ExperimentalAnimationApi
 @ExperimentalMaterialApi
@@ -28,8 +32,16 @@ fun NavGraphBuilder.listComposable(
     ) { navBackStackEntry ->
         val action = navBackStackEntry.arguments?.getString(LIST_ARGUMENT_KEY).toAction()
 
-        LaunchedEffect(key1 = action) {
-            sharedViewModel.action.value = action
+        // this value will be saved across configuration changes
+        var myAction by rememberSaveable {
+            mutableStateOf(Action.NO_ACTION)
+        }
+
+        LaunchedEffect(key1 = myAction) {
+            if(action != myAction){
+                myAction = action
+                sharedViewModel.action.value = action
+            }
         }
 
         //observing an action
